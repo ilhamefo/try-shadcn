43f6b19db430b532f335f4942b229a8e4d60ad5f
Efo <ilham@oninyon.com>
2024-12-20 15:34:44 +0700

    - first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AppSidebar from './components/AppSidebar.vue';
+import SidebarProvider from './components/ui/sidebar/SidebarProvider.vue';
+import SidebarTrigger from './components/ui/sidebar/SidebarTrigger.vue';
+
+
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarTrigger />
+  </SidebarProvider>
+</template>
---
+++ components/AppSidebar.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import DropdownMenu from "./ui/dropdown-menu/DropdownMenu.vue";
+import DropdownMenuTrigger from "./ui/dropdown-menu/DropdownMenuTrigger.vue";
+import { ChevronUp, Cloud, CreditCard, Github, Keyboard, LifeBuoy, LogOut, Mail, MessageSquare, Plus, PlusCircle, Settings, User, User2, UserPlus, Users } from "lucide-vue-next";
+import DropdownMenuContent from "./ui/dropdown-menu/DropdownMenuContent.vue";
+import DropdownMenuItem from "./ui/dropdown-menu/DropdownMenuItem.vue";
+import DropdownMenuLabel from "./ui/dropdown-menu/DropdownMenuLabel.vue";
+import DropdownMenuSeparator from "./ui/dropdown-menu/DropdownMenuSeparator.vue";
+import DropdownMenuGroup from "./ui/dropdown-menu/DropdownMenuGroup.vue";
+import DropdownMenuShortcut from "./ui/dropdown-menu/DropdownMenuShortcut.vue";
+import DropdownMenuSub from "./ui/dropdown-menu/DropdownMenuSub.vue";
+import DropdownMenuSubTrigger from "./ui/dropdown-menu/DropdownMenuSubTrigger.vue";
+import { DropdownMenuPortal } from "radix-vue";
+import DropdownMenuSubContent from "./ui/dropdown-menu/DropdownMenuSubContent.vue";
+
+const items = ref([
+    {
+        title: "Home",
+        url: "#",
+        // icon: Home,
+    },
+    {
+        title: "Inbox",
+        url: "#",
+        // icon: Inbox,
+    },
+    {
+        title: "Calendar",
+        url: "#",
+        // icon: Calendar,
+    },
+    {
+        title: "Search",
+        url: "#",
+        // icon: Search,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        // icon: Settings,
+    },
+])
+
+
+</script>
+
+<template>
+    <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel>Apps</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem v-for="item in items" :key="item.title">
+                            <SidebarMenuButton>
+                                <a href="#">{{ item.title }}</a>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+
+        <SidebarFooter>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <SidebarMenuButton>
+                                <User2 /> Username
+                                <ChevronUp class="ml-auto" />
+                            </SidebarMenuButton>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent class="w-56">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <User class="mr-2 h-4 w-4" />
+                                    <span>Profile</span>
+                                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <CreditCard class="mr-2 h-4 w-4" />
+                                    <span>Billing</span>
+                                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Settings class="mr-2 h-4 w-4" />
+                                    <span>Settings</span>
+                                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Keyboard class="mr-2 h-4 w-4" />
+                                    <span>Keyboard shortcuts</span>
+                                    <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuGroup>
+                                <DropdownMenuItem>
+                                    <Users class="mr-2 h-4 w-4" />
+                                    <span>Team</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuSub>
+                                    <DropdownMenuSubTrigger>
+                                        <UserPlus class="mr-2 h-4 w-4" />
+                                        <span>Invite users</span>
+                                    </DropdownMenuSubTrigger>
+                                    <DropdownMenuPortal>
+                                        <DropdownMenuSubContent>
+                                            <DropdownMenuItem>
+                                                <Mail class="mr-2 h-4 w-4" />
+                                                <span>Email</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                <MessageSquare class="mr-2 h-4 w-4" />
+                                                <span>Message</span>
+                                            </DropdownMenuItem>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>
+                                                <PlusCircle class="mr-2 h-4 w-4" />
+                                                <span>More...</span>
+                                            </DropdownMenuItem>
+                                        </DropdownMenuSubContent>
+                                    </DropdownMenuPortal>
+                                </DropdownMenuSub>
+                                <DropdownMenuItem>
+                                    <Plus class="mr-2 h-4 w-4" />
+                                    <span>New Team</span>
+                                    <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                                </DropdownMenuItem>
+                            </DropdownMenuGroup>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <Github class="mr-2 h-4 w-4" />
+                                <span>GitHub</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <LifeBuoy class="mr-2 h-4 w-4" />
+                                <span>Support</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem disabled>
+                                <Cloud class="mr-2 h-4 w-4" />
+                                <span>API</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <LogOut class="mr-2 h-4 w-4" />
+                                <span>Log out</span>
+                                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarFooter>
+    </Sidebar>
+</template>
